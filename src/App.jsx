55763806533e813d2305196884bafdbe0cd6514f
alex <alex.krysiuk@icloud.com>
2023-08-05 20:32:16 +0200

    Install Bootstrap 5

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Header from './Header';
 import Content from './Content';
 import Footer from './Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
---
+++ src/Employees.jsx
@@ -0,0 +1,8 @@
+const Employees = () => {
+    return(
+        <main>
+            <h1>Employees</h1>
+        </main>
+    )
+}
+export default Employees
