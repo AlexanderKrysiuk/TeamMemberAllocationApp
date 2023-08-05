11fddfa16683fec1e4addd813ca9f30b7354d5c9
alex <alex.krysiuk@icloud.com>
2023-08-05 21:46:41 +0200

    Create JSX Code to Render Bootstrap Cards for Employees

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -90,13 +90,22 @@ const Employees = () => {
     ]);
     return(
         <main className="container">
-            <div>
-                <div>
-                    {
-                        employees.map((employees) => (
-                            <p>{employees.fullName}</p>
+            <div class="row justify-content-center mt-3 mb-3">
+                <div class="col-8">
+                    <div class="card-collection">
+                        {
+                            employees.map((employee) => (
+                                <div id={employee.id} className="card m-2" style={{cursor:"pointer"}}>
+                                    {(employee.gender === 'male')?<img src={maleProfile} className="card-img-top"/>
+                                                                 :<img src={femaleProfile} className="card-img-top"/>}
+                                    <div className="card-body">
+                                        <h5 className="card-title">Full Name: {employee.fullName}</h5>
+                                        <p className="card-text"><b>Designation:</b> {employee.designation}</p>
+                                    </div>    
+                                </div>
                             ))
-                    }
+                        }
+                    </div>
                 </div>
             </div>
         </main>
